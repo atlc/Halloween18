2aecf3d3ea4431d785d8f58aa86c400462bc94a5
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2018-10-17 22:20:47 -0500

    Hardcoding the 4 side rings and the top bulb ring because WHO SCHEDULES A HALLOWEEN PARTY FOR THE 19TH

--- AnglerFish/AnglerFish.ino
+++ AnglerFish/AnglerFish.ino
@@ -1,4 +1,4 @@
-#include <Adafruit_NeoPixel.h>
+   #include <Adafruit_NeoPixel.h>
 
 /* 
  * 16G wire colors:
@@ -6,91 +6,175 @@
  *    Brown:  Ground
  *    Yellow: 5v  
  * 
- * Treating the lighting as one long Neopixel array of 89 lights, this is how the placement and indices are laid out
+ * Treating the lighting as one long Neopixel array of 68 lights, this is how the placement and indices are laid out
  * 
- * Lighting Object      |   Indices   |  Quantity LEDs 
+ * Lighting Object          | Indices |  Quantity LEDs 
  * ---------------------------------------------------
- * Left FRONT (lg ring)     [  0 - 15 ]       16
- * Left REAR (sm ring)      [ 16 - 27 ]       12
- * Right REAR (sm ring)     [ 28 - 39 ]       12
- * Right FRONT (lg ring)    [ 40 - 55 ]       16
- * SPINE atop the fish      [ 56 - 76 ]       21
- * Bulb                     [ 77 - 88 ]       12
+ * Right FRONT (lg ring)     [  0 - 15 ]       16
+ * Right REAR (sm ring)      [ 16 - 27 ]       12
+ * Left  REAR (sm ring)      [ 28 - 39 ]       12
+ * Left  FRONT (lg ring)     [ 40 - 55 ]       16
+ * Bulb                      [ 56 - 67 ]       12
  * ---------------------------------------------------
 */
 
-// #define N_LEDS 42
-
-
 #define PIN    6
-#define N_LEDS 12
-
+#define N_LEDS 68
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(N_LEDS, PIN, NEO_GRB + NEO_KHZ800);
-#define MY_COLOR strip.Color(0,0,40)
 
+#define MY_COLOR strip.Color(25,5,0)
 
 void setup() {
   strip.begin();
 }
 
 void loop() {
- spine();
-// fins();1
+ pulse();
+ delay(750);
 }
 
-static void spine() {
-  for(uint16_t i=0; i<N_LEDS+5; i++) {
-      strip.setPixelColor(i, MY_COLOR); // Draw new pixel at "front"
-      strip.setPixelColor(i-5, 0); // Erase pixel a few steps back
-      strip.show();
-      delay(65);
+// Iterates through the 4 rings to create a pulsing effect
+static void pulse() {
+  for(uint16_t i=0; i<N_LEDS; i++) {
+    strip.setPixelColor(i, strip.Color(35,7,0));
   }
-}
-
-static void fins() {
- strip.setPixelColor(21, MY_COLOR); // Left fin
- strip.setPixelColor(102, MY_COLOR); // Right fin
- strip.show();
- delay(85);
- strip.clear(); // Both fins off
- delay(85);
-}
-
-static void fronts() {
-  for(uint16_t i=22; i<37; i++) {
-    strip.setPixelColor(i, MY_COLOR); // Fill in Left Front ring
-  }
-  for(uint16_t i=86; i<101; i++) {
-    strip.setPixelColor(i, MY_COLOR); // Fill in Right Front ring
-  }
-
   strip.show();
-  delay(85);
-  strip.clear();
-  delay(85);
-}
+  delay(40);
 
-static void mids() {
-  for(uint16_t i=38; i<49; i++) {
-    strip.setPixelColor(i, MY_COLOR); // Fill in Left Middle ring
+    for(uint16_t i=0; i<N_LEDS; i++) {
+    strip.setPixelColor(i, strip.Color(32,7,0));
   }
-  for(uint16_t i=74; i<85; i++) {
-    strip.setPixelColor(i, MY_COLOR); // Fill in Right Middle ring
-  }
-
   strip.show();
-  delay(85);
-  strip.clear();
-  delay(85);
-}
+  delay(40);
 
-static void rears() {
-  for(uint16_t i=50; i<73; i++) {
-    strip.setPixelColor(i, MY_COLOR); // Fill in Left Middle ring
+    for(uint16_t i=0; i<N_LEDS; i++) {
+    strip.setPixelColor(i, strip.Color(32,6,0));
   }
-
   strip.show();
-  delay(85);
-  strip.clear();
-  delay(85);
+  delay(40);
+
+  for(uint16_t i=0; i<N_LEDS; i++) {
+    strip.setPixelColor(i, strip.Color(30,6,0));
+  }
+  strip.show();
+  delay(40);
+
+  for(uint16_t i=0; i<N_LEDS; i++) {
+    strip.setPixelColor(i, strip.Color(27,6,0));
+  }
+  strip.show();
+  delay(40);
+
+  for(uint16_t i=0; i<N_LEDS; i++) {
+    strip.setPixelColor(i, strip.Color(27,5,0));
+  }
+  strip.show();
+  delay(40);
+
+  for(uint16_t i=0; i<N_LEDS; i++) {
+    strip.setPixelColor(i, strip.Color(25,5,0));
+  }
+  strip.show();
+  delay(40);
+
+  for(uint16_t i=0; i<N_LEDS; i++) {
+    strip.setPixelColor(i, strip.Color(23,5,0));
+  }
+  strip.show();
+  delay(40);
+
+  for(uint16_t i=0; i<N_LEDS; i++) {
+    strip.setPixelColor(i, strip.Color(22,4,0));
+  }
+  strip.show();
+  delay(40);
+
+  for(uint16_t i=0; i<N_LEDS; i++) {
+    strip.setPixelColor(i, strip.Color(20,4,0));
+  }
+  strip.show();
+  delay(40);
+
+    for(uint16_t i=0; i<N_LEDS; i++) {
+    strip.setPixelColor(i, strip.Color(17,4,0));
+  }
+  strip.show();
+  delay(40);
+
+      for(uint16_t i=0; i<N_LEDS; i++) {
+    strip.setPixelColor(i, strip.Color(16,3,0));
+  }
+  strip.show();
+  delay(40);
+
+  for(uint16_t i=0; i<N_LEDS; i++) {
+    strip.setPixelColor(i, strip.Color(15,3,0));
+  }
+  strip.show();
+  delay(40);
+
+    for(uint16_t i=0; i<N_LEDS; i++) {
+    strip.setPixelColor(i, strip.Color(13,3,0));
+  }
+  strip.show();
+  delay(40);
+
+      for(uint16_t i=0; i<N_LEDS; i++) {
+    strip.setPixelColor(i, strip.Color(12,2,0));
+  }
+  strip.show();
+  delay(40);
+
+  for(uint16_t i=0; i<N_LEDS; i++) {
+    strip.setPixelColor(i, strip.Color(10,2,0));
+  }
+  strip.show();
+    delay(40);
+
+      for(uint16_t i=0; i<N_LEDS; i++) {
+    strip.setPixelColor(i, strip.Color(7,2,0));
+  }
+  strip.show();
+  delay(40);
+
+      for(uint16_t i=0; i<N_LEDS; i++) {
+    strip.setPixelColor(i, strip.Color(7,1,0));
+  }
+  strip.show();
+  delay(40);
+  
+  for(uint16_t i=0; i<N_LEDS; i++) {
+    strip.setPixelColor(i, strip.Color(5,1,0));
+  }
+  strip.show();
+  delay(40);
+
+  for(uint16_t i=0; i<N_LEDS; i++) {
+    strip.setPixelColor(i, strip.Color(4,1,0));
+  }
+  strip.show();
+  delay(40);
+
+  for(uint16_t i=0; i<N_LEDS; i++) {
+    strip.setPixelColor(i, strip.Color(3,1,0));
+  }
+  strip.show();
+  delay(40);
+
+  for(uint16_t i=0; i<N_LEDS; i++) {
+    strip.setPixelColor(i, strip.Color(2,0,0));
+  }
+  strip.show();
+  delay(40);
+
+for(uint16_t i=0; i<N_LEDS; i++) {
+    strip.setPixelColor(i, strip.Color(1,0,0));
+  }
+  strip.show();
+  delay(40);
+
+  for(uint16_t i=0; i<N_LEDS; i++) {
+    strip.setPixelColor(i, strip.Color(0,0,0));
+  }
+  strip.show();
 }
